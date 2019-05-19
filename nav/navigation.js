22375bcba04d5f6390b09c9e33e58fa7ac84f1c8
Roger Lee <rmlee0003@gmail.com>
2019-05-19 14:30:30 -0400

    navigation.less added styling for side nav

---
+++ nav/navigation.js
@@ -0,0 +1,5 @@
+let blurDiv = document.querySelector('#navBlurDiv');
+let sideNav = document.querySelector('#sideNavDiv');
+
+let menu = document.querySelector('.menu');
+let closeMenu = document.querySelector('.menu-close');
