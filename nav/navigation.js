90ef66378adfeaf6af1a7903ae28dd36265096e0
Roger Lee <rmlee0003@gmail.com>
2019-05-19 14:41:52 -0400

    navigation.js added sideNav functionality

--- nav/navigation.js
+++ nav/navigation.js
@@ -3,3 +3,19 @@ let sideNav = document.querySelector('#sideNavDiv');
 
 let menu = document.querySelector('.menu');
 let closeMenu = document.querySelector('.menu-close');
+
+menu.addEventListener('click', function(){
+ sideNav.classList.remove('side-hide');
+ sideNav.classList.add('side-show');
+ blurDiv.classList.add('nav-blur');
+ blurDiv.classList.remove('side-hide');
+});
+
+
+closeMenu.addEventListener('click', function(){
+ sideNav.classList.remove('side-show');
+ sideNav.classList.add('side-hide');
+ blurDiv.classList.remove('nav-blur');
+ blurDiv.classList.add('side-hide');
+
+});
