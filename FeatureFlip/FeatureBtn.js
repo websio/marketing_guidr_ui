42768f3e4b1fa7ddc0c997b925f0ac0d49b95934
Michael Williams <manicdesigner@gmail.com>
2019-05-22 21:00:50 -0400

    eventlistener(changebutton) change button changes card on button click all text and everything inside flip color

--- FeatureFlip/FeatureBtn.js
+++ FeatureFlip/FeatureBtn.js
@@ -6,10 +6,28 @@ class Card {
         this.cardPars = this.element.querySelectorAll('.card-p'); // node list
         this.button = this.element.querySelector('.card-btn');
 
+        
+        this.button.addEventListener('click', () => this.changeColors());
 
     }
     changeColors() {
-        
+        if(!this.background.classList.contains('card-bg-alt')) {
+
+            this.background.classList.add('card-bg-alt');
+            this.header.classList.add('card-header-alt');
+            this.cardPars.forEach(par => {
+                par.classList.add('card-pars-alt');               
+            });
+            this.button.classList.add('card-btn-alt');
+        } else {
+            this.background.classList.remove('card-bg-alt');
+            this.header.classList.remove('card-header-alt');
+            this.cardPars.forEach(par => {
+                par.classList.remove('card-pars-alt');               
+            });
+            this.button.classList.remove('card-btn-alt');
+            
+        }
     }
 }
 
